ade689c8bfcfc65b9b135f484983b09ac177871f
sarmad505 <50269577+sarmad505@users.noreply.github.com>
2021-03-07 02:29:58 +0500

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,14 @@
+Percentage = float(input("ENTER YOUR PERCENTAGE = "))
+
+if Percentage >= 79:
+    print("Congrats , your Grade is A+")
+elif Percentage >= 69:
+    print("Congrats , your Grade is A")
+elif Percentage >= 59:
+    print("Congrats , your Grade is B")
+elif Percentage >= 49:
+    print("your Grade is C")
+elif Percentage >= 39:
+    print("your Grade is D")
+else:
+    print("your Grade is F")
